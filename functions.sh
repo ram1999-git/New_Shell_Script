cdc389d359e5d93f4bcc0c4631ca53ad1d237fc4
Ram Chaitanya <ramchaithanya1999@gmail.com>
2025-09-09 16:54:59 +0530

    This is logfile

--- functions.sh
+++ functions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 userid=$(id -u)
+Timestamp=$(date +%F-%H-%M-%S)
+Script_Name=$($0 | awk -d "." -f1)
+logfile =/temp/$Script_Name-$Timestamp.logss
 
 validate(){
     if [$1 -ne 0]
@@ -19,7 +22,7 @@ else
   echo "You are super user"
 fi
 
-dnf install mysql -y
+dnf install mysql -y &>>$logfile
 
 validate $? "Installing mysql"
 
@@ -28,7 +31,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-dnf install git -y
+dnf install git -y &>>$logfile
 
 if [ $? -ne 0 ]; then 
   echo "Installation of git is Failure"
