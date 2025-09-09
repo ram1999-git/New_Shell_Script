dc870ca2b8517e27758ecf9f600c940448875d8c
Ram Chaitanya <ramchaithanya1999@gmail.com>
2025-09-09 15:11:57 +0530

    This is conditions

---
+++ conditons.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID =$(id =0)
+ if($USERID -eq 0)
+  then
+     echo "You are a super user"
+  else
+     echo "please run as a super user"
+     exit 1
+  fi
+
+dnf install mysql -y
+  if ($? -ne 0)
+   then 
+     echo "Installation of mysql failed"
+     exit 1
+   else 
+     echo "Mysql Installation sucess"
+
+dnf insal git -y
+  if ($? -ne 0)
+   then 
+     echo "Installation of git failed"
+     exit 1
+   else 
+     echo "git Installation sucess"
+
