61a192a741d6d40aabb10d83b9f46f202d988a2b
Ram Chaitanya <ramchaithanya1999@gmail.com>
2025-09-09 15:33:58 +0530

    This is conditions

--- conditons.sh
+++ conditons.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-Userid = $(id -u)
- if[$Userid -ne 0]
-  then
-     echo "You are a super user"
-  else
-     echo "please run as a super user"
-     exit 1
-  fi
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "Please run as a superuser (use sudo)"
+    exit 1
+else
+    echo "You are a superuser"
+fi
 
+# Install MySQL
 dnf install mysql -y
-  if ($? -ne 0)
-   then 
-     echo "Installation of mysql failed"
-     exit 1
-   else 
-     echo "Mysql Installation sucess"
+if [ $? -ne 0 ]; then
+    echo "Installation of MySQL failed"
+    exit 1
+else
+    echo "MySQL installation successful"
+fi
 
-dnf insal git -y
-  if ($? -ne 0)
-   then 
-     echo "Installation of git failed"
-     exit 1
-   else 
-     echo "git Installation sucess"
-
+# Install Git
+dnf install git -y
+if [ $? -ne 0 ]; then
+    echo "Installation of Git failed"
+    exit 1
+else
+    echo "Git installation successful"
+fi
