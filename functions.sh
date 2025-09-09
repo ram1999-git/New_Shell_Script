51dcac2165fdf51c9693a88fcd60e933a64b7175
Ram Chaitanya <ramchaithanya1999@gmail.com>
2025-09-09 15:38:06 +0530

    This is functions

---
+++ functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+validate(){
+   echo "Exit status : $1"
+}
+
+if [ $USERID -ne 0 ];
+ then
+    echo "Please run as a superuser (use sudo)"
+    exit 1
+else
+    echo "You are a superuser"
+fi
+
+# Install MySQL
+dnf install mysql -y
+if [ $? -ne 0 ]; then
+    echo "Installation of MySQL failed"
+    exit 1
+else
+    echo "MySQL installation successful"
+fi
+VALIDATE $? "iNSTALLED MYSQL"
